18c9e484a172ca3d618bdf6917c2c9195850f7f6
seokyang <uoon97@gmail.com>
2023-07-14 00:20:11 +0900

    [Add] save_images service & random_number_img service

---
+++ app/service_1.py
@@ -0,0 +1,33 @@
+import os
+import pickle
+import numpy as np
+from PIL import Image
+
+image_path = os.environ.get('IMAGEPATH')
+if image_path is None:
+    image_path = '../data'
+
+print(image_path)
+
+def load_data():
+
+    numbers = range(0, 10)
+    images = []
+
+    for n in numbers:
+        globals()[f'fname_{n}'] = os.listdir(os.path.join(image_path, f'{n}'))
+
+    for n in numbers:
+            for i in range(len(globals()[f'fname_{n}'])):
+                f = os.path.join(image_path, f'{n}', globals()[f'fname_{n}'][i])
+                image = Image.open(f)
+                image = image.resize((224, 224))
+                arr = np.array(image)
+                images.append(arr)
+
+    return images
+
+images = load_data()
+
+with open(file = '../model/service_1.pickle', mode = 'wb') as f:
+     pickle.dump(images, f)
---
+++ app/service_2.py
@@ -0,0 +1,40 @@
+import os
+import pickle
+from io import BytesIO
+import numpy as np
+import matplotlib.pyplot as plt
+from flask import Flask, send_file
+
+model_path = os.environ.get('MODELPATH')
+if model_path is None:
+    model_path = '../model'
+
+with open(file = os.path.join(model_path, 'service_1.pickle'), mode = 'rb') as f:
+          images = pickle.load(f)
+
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def index():
+    
+    samples = np.random.randint(len(images), size = 9)
+    result = []
+
+    for sample in samples:
+        result.append(images[sample])
+    
+    plt.figure(figsize = (8, 8))
+    for idx, image in enumerate(result):
+        plt.subplot(3, 3, idx+1)
+        plt.imshow(image, cmap = 'gray')
+        plt.axis('off')
+    
+    img = BytesIO()
+    plt.savefig(img, format = 'png', dpi = 200)
+    img.seek(0)
+    return send_file(img, mimetype = 'image/png')
+
+if __name__ == '__main__':
+     app.run(host='0.0.0.0', port = 80)
