c5365451df01e13f7d52885c97a1ca91077ebe8a
uoon97 <64677725+uoon97@users.noreply.github.com>
2023-07-14 09:14:07 +0900

    Update test_service.py

--- test_service.py
+++ test_service.py
@@ -5,7 +5,7 @@ from flask import Flask, send_file
 
 model_path = os.environ.get('MODELPATH')
 if model_path is None:
-    model_path = '../model'
+    model_path = 'model'
 
 with open(file = os.path.join(model_path, 'service_1.pickle'), mode = 'rb') as f:
     images = pickle.load(f)
