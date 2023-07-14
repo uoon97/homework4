74794794429be4d0777888317167d2ae333ac534
seokyang <uoon97@gmail.com>
2023-07-14 09:29:32 +0900

    [Edit] test_service.py

--- test_service.py
+++ test_service.py
@@ -1,12 +1,13 @@
 import os
 import pickle
+import numpy as np
 
 model_path = os.environ.get('MODELPATH')
 if model_path is None:
     model_path = 'model'
 
 with open(file = os.path.join(model_path, 'service_1.pickle'), mode = 'rb') as f:
-    images = pickle.load(f)
+    images = np.array(pickle.load(f))
 
 def test_image():
     image = images[0]
